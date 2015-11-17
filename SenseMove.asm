02615ee1b6787784b5b95ef54d53e22f5e670037
Marie Bomber <m.bomber2@students.clark.edu>
2015-11-17 12:49:48 -0800

    Added testing for L/R/Both sensor use, added testing for turn/straight motion through sense

--- SenseMove.asm
+++ SenseMove.asm
@@ -13,34 +13,41 @@
 	    
 	    GLOBAL	    SMOVE
 	    
-SMOV:	    code	    0x400
-SMOVE:	    CALL	    GROUND			    ; Ground echo pins to insure proper functionality of sensor
-	    BTFSC	    SMOVCON
+SMOV:	   code		    0x400
+SMOVE:	   CALL		    GROUND			    ; Ground echo pins to insure proper functionality of sensor
+	   MOVLW	    0x05	        
+	   MOVWF	    WREG			    ; Prepare WREG for sensor set up
+	   BTFSC	    SMOVCON,	    2		    ; Test for use of R Sensor
+	   BCF		    WREG,	    0		    ; If R sensor only is needed, clear L sensor trigger bit
+	   BTFSC	    SMOVCON,	    3		    ; Test for use of L Sensor
+	   BCF		    WREG,	    3		    ; If L sensor only is needed, clear R sensor trigger bit
+	   MOVWF	    PORTB			    ; Set Trigger ~10 us before sense loop begins
+	   MOVLW	    0x0F			    ; Default to forward motion (1 cycle)
+	   BTFSS	    SMOVCON,	    0		    ; Test for R turn		(2-3 cycle)
+	   BCF		    WREG,	    2
+	   BTFSS	    SMOVCON,	    1		    ; Test for L turn		(4-5 cycle)
+	   BCF		    WREG,	    1		    
+	   MOVWF	    PORTA			    ; Begin motion		(6 cycle)
+	   CLRF		    TEMPR			    ; Clear R sensor count	(7 cycle)
+	   CLRF		    TEMPL			    ; Clear L sensor count	(8 cycle)
+	   
+	   
+	   
+	   
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-GROUND:	   BTFSC	    SMOVCON,	    2		    ; Test for use of right sensor
+GROUND:	   BTFSC	    SMOVCON,	    2		    ; Test for use of R sensor
 	   BCF		    TRISB,	    RB2		    ; Ground R. Echo
-	   BTFSC	    SMOVCON,	    3		    ; Test for use of left sensor
+	   BTFSC	    SMOVCON,	    3		    ; Test for use of L sensor
 	   BCF		    TRISB,	    RB1		    ; Ground L. Echo
 	   MOVLW	    0x06
 	   CALL		    GRDELAY			    ; Delay ~25 us
 	   MOVLW	    0x0A			    
 	   MOVWF	    TRISB			    ; Set R and L echo back to input
+	   RETURN
 	   
-GRDELAY:    DECF	    WREG
-	    BNZ		    GRDELAY
-	    RETURN
+GRDELAY:   DECF	    WREG
+	   BNZ		    GRDELAY
+	   RETURN
 	    
 
