bdca12ef42d9fb0e63736f1ef4eac58e01735d9a
Johnathan Bunn <me@tuxrox.net>
2016-03-17 22:22:38 -0700

    Initial test, turns out git is built in but is different than the default versioning system. interesting.

---
+++ robot.asm
@@ -0,0 +1,312 @@
+#include p18f1220.inc 
+    
+global RobotDoLoop
+global RobotDoInit
+
+_SensCount equ 0x88
+_PWMCount equ 0x89
+PWMCONL equ 0x090
+PWMCONR equ 0x091
+PWMCOUNT equ 0x092
+PWMONL equ 0x093
+PWMONR equ 0x094
+
+SensStatus equ 0x95
+SensCount  equ 0x96
+SensLastL equ 0x97
+SensLastR equ 0x98
+ 
+StatusTrig equ 0
+StatusSkip equ 1
+StatusCount equ 2
+StatusDone equ 3
+
+SensPort equ PORTB
+TrigL equ RB0
+EchoL equ RB1
+TrigR equ RB2
+EchoR equ RB3
+
+PWMPORT	equ PORTA
+PWMRIN	equ RA0
+PWMRCE	equ RA1
+
+PWMLIN	equ RA2
+PWMLCE	equ RA3
+		
+pwm code 0x400
+  
+RobotDoInit:
+	    
+    ; Clear PWM Variables
+    clrf PWMCONL
+    clrf PWMCONR
+    clrf PWMCOUNT
+    clrf PWMONL
+    clrf PWMONR
+    
+    ; clear robot loop vars
+    clrf _PWMCount
+    clrf _SensCount
+    clrf SensStatus
+    clrf SensCount
+    clrf SensLastL
+    clrf SensLastR
+    
+    movlw 0x00	
+    movwf TRISA ; set PortA To output.
+    clrf PORTA ; Clear port a to make sure that the motors start stopped.
+    
+    movlw 0x0A
+    movwf TRISB ; setup inputs and outputs for PORT B for Sensor Triggering
+    clrf PORTB;
+    bcf PORTB,RB5
+    
+    ; Timer 2 Initialization + interrupt enable/disable
+    bsf PIE1, TMR2IE ; enable Timer1 Interrupt
+    bsf IPR1, TMR2IP ; Set Timer 1 Interrupt to High priority
+     
+    ; TMR2 is used for pwm so it works here too
+    clrf TMR2 
+    movlw 0x00 
+    movwf T2CON 
+    movlw 0xFA
+    movwf PR2
+    
+    bsf T2CON,TMR2ON
+    return 
+ 
+RobotDoLoop:
+    incf _PWMCount
+    movlw .4
+    cpfslt _PWMCount
+    call PWMLoop
+    
+    incf _SensCount
+    movlw .120
+    cpfslt _SensCount
+    bra SensTrigger
+    
+    btfsc SensStatus,StatusTrig ; if trigger is set then we need to stop it
+    call SensEndTrigger
+    
+    btfsc SensStatus,StatusSkip ; skip this cycle to give it a chance to display
+    bra SkipCount
+    
+    btfsc SensStatus,StatusCount ; we should either be counting or waiting here, about to see
+    call SensRead
+    
+    btfsc SensStatus,StatusDone ; we should now publish to the proper register
+    call SensPublish
+ ;   btfsc SensR,6
+ ;   call SensReadR ; if trigger is set read that side
+ 
+RobotLoopDone:
+    bcf PIR1, TMR2IF ; Clear Timer 1 Interrupt Flag
+    return
+
+SensPublish:
+    btfss SensStatus,7
+    bra PublishL
+    bra PublishR
+PublishL:
+    movff SensCount,SensLastL
+    bcf SensStatus,StatusDone
+    return
+PublishR:
+    movff SensCount,SensLastR
+    bcf SensStatus,StatusDone
+    return
+    
+SkipCount:
+    btfss SensStatus,StatusCount
+    bra SkipAgain
+    
+    bcf SensStatus,StatusTrig
+    bcf SensStatus,StatusSkip
+    bsf SensStatus,StatusCount
+    bra RobotLoopDone
+    
+SkipAgain:
+    bsf SensStatus,StatusCount
+    bra RobotLoopDone
+    
+SensEndTrigger:
+    bcf SensPort,TrigL
+    bcf SensPort,TrigR
+    bcf SensStatus,StatusTrig ; turn off trigger state
+    bsf SensStatus,StatusSkip ; turn on skip state
+    return
+    
+SensTrigger:
+    clrf _SensCount ; esnure we get here again
+    ;Determine which is on, if Status is 0 we turn right on, if status is 1 we turn left on
+    btfsc SensStatus,7 ; 0 is left 1 is right
+    bra TriggerLeft
+    bra TriggerRight
+
+TriggerLeft:
+    ;Check if right sensor is still on
+    ;Right Sensor has had it's chance, time to kill it if it isn't allready done
+    btfsc SensPort,EchoR ; if this is still high we need to kill it now before we continue
+    call KillRight
+
+    bsf SensPort,EchoL ; Set input to recieve echo
+    bsf SensPort,TrigL ; Set Trigger
+    
+    ; set status 
+    clrf SensStatus
+    ; no need to set any bits since 0 is left
+    
+    bra TriggerDone
+ 
+TriggerRight:
+    ;Check if left sensor is still on
+    ;leftt Sensor has had it's chance, time to kill it if it isn't allready done
+    btfsc SensPort,EchoL ; if this is still high we need to kill it now before we continue
+    call KillLeft
+
+    bsf SensPort,EchoR ; Set input to recieve echo
+    bsf SensPort,TrigR ; Set Trigger
+    
+    ; set status 
+    clrf SensStatus
+    bsf SensStatus,7 ; set this to 1 for right
+    
+    bra TriggerDone
+
+TriggerDone: 
+    bsf SensStatus,StatusTrig ; this set's trigger stop state we need to clean
+    ; up and stop the sensor on the next cycle
+    clrf SensCount
+    bra RobotLoopDone
+
+KillRight:
+    ;set echo to output and clear
+    bcf TRISB,EchoR
+    bcf SensPort,EchoR
+    bsf TRISB,EchoR
+    setf SensLastR
+    return
+    
+KillLeft:
+    ;set echo to output and clear
+    bcf TRISB,EchoL
+    bcf SensPort,EchoL
+    bsf TRISB,EchoL
+    setf SensLastL
+    return
+
+SensRead:
+    btfss SensStatus,7
+    bra SensReadL
+    bra SensReadR
+    
+SensReadL:
+    ;check if trigger is on
+    btfss SensPort,EchoL
+    bra clearTrigger ; it went off sometime between the last update and now so we do not increment
+    incf SensCount
+    bra SensDone
+    
+SensReadR:
+     ;check if trigger is on
+    btfss SensPort,EchoR
+    bra clearTrigger ; it went off sometime between the last update and now so we do not increment
+    incf SensCount
+    bra SensDone
+    
+clearTrigger:
+    bcf SensStatus,StatusCount ; no longer counting
+    bsf SensStatus,StatusDone ; should now publish on next cycle
+    bra SensDone
+    
+SensDone:    
+    return
+PWMLoop:
+    clrf _PWMCount ; ensure we get here again
+    tstfsz  PWMCOUNT
+    bra PWMUPDATE
+    bra PWMInit
+    
+PWMInit:
+    ; do init 
+    movlw   0x32
+    movwf   PWMCOUNT
+    
+    ;get loncount
+    movf    PWMCONL,0
+    andlw   0x1F
+    addlw   .6	; this is to make 1 a usable setting
+    MOVWF   PWMONL
+    
+    ;get roncount
+    MOVF   PWMCONR,0
+    ANDLW   0x1F
+    addlw   .6	; this is to make 1 a usable setting
+    MOVWF   PWMONR
+    
+    BCF	PWMPORT,PWMLIN
+    BTFSC PWMCONL,6
+    BSF PWMPORT,PWMLIN
+    
+    BCF	PWMPORT,PWMRIN
+    BTFSC PWMCONR,6
+    BSF PWMPORT,PWMRIN
+    
+    BRA	    PWMDONE
+    
+PWMUPDATE:
+    ; update Left motor
+    BTFSS PWMCONL,7
+    BRA PWMLeftNop
+  
+    BTFSS PWMCONL,5
+    BRA PWMTurnLOn
+    
+    TSTFSZ PWMONL
+    BRA PWMDecL
+   
+    BCF PWMPORT,PWMLCE
+    BRA PWMLeftDone
+PWMLeftNop:
+    NOP
+    NOP
+    NOP
+    NOP ; these nops are to balance timing so all cycles are the same
+    BRA PWMLeftDone
+PWMDecL: DECF PWMONL,1
+	NOP ; to balance
+PWMTurnLOn: BSF PWMPORT,PWMLCE
+PWMLeftDone:   
+    ; update Right motor
+    BTFSS PWMCONR,7
+    BRA PWMRightNop
+    BTFSS PWMCONR,5
+    BRA PWMTurnROn
+    
+    TSTFSZ PWMONR
+    BRA PWMDecR
+   
+    BCF PWMPORT,PWMRCE
+    BRA PWMRightDone
+PWMRightNop:
+    nop
+    nop
+    nop
+    nop ; these nops are to balance timing so all cycles are the same
+    bra PWMRightDone
+PWMDecR: DECF PWMONR,1  
+    nop ; to balance
+PWMTurnROn: BSF PWMPORT,PWMRCE
+PWMRightDone:   
+
+    MOVLW 0x16	; 156 should be 100 cycles
+    MOVWF TMR2
+    
+    decf PWMCOUNT,1
+PWMDONE:
+    return
+
+
+end
---
+++ robotTest.asm
@@ -0,0 +1,106 @@
+;-------------------------------------------------------------------------------------
+; FILE: robotTest.asm
+; DESC: Test Robot library, should stop motion before hitting wall
+; DATE: 3.7.2016 
+; AUTH: Johnathan Bunn
+; DEVICE: PICmicro (PIC18F1220)
+;-------------------------------------------------------------------------------------
+    list p=18F1220		; Set processor type
+    radix hex			; Sets the default radix for data exp.
+    config WDT=OFF, LVP=OFF	; Disable Watchdog timer and Low Voltage
+    config	   OSC= INTIO2
+
+#include p18f1220.inc 
+#include robot.inc
+   
+RobotState equ 0x80
+LastSensL equ 0x81
+CurSensL equ 0x82
+ 
+start   code 0x000 ; Executes after reset
+    goto INIT
+    
+high    code 0x008 ; Executes after high priority interrupt
+    goto HPRIO
+    
+main    code 0x020
+HPRIO:
+    btfsc PIR1, TMR2IF ; high priority loop
+    bra RobotL
+   
+    retfie ; Return from interrupt
+
+RobotL: call RobotDoLoop ; PWM_LOOP is responsible to clear the flag.
+    bra HPRIO	; return to HPRIO in case other interrupts need to be processed.
+
+    
+INIT:
+    movlw   0x60	
+    iorwf   OSCCON	; Set to 4mhz
+    movlw   0x7F	; Set all A\D Converter Pins as
+    movwf   ADCON1	; digital I/O pins
+    
+    ; Enable interupts and get ready for sub init code
+    bsf INTCON, GIE ; enable interrupts
+    bsf INTCON, PEIE ; enable all interrupts
+    
+    ;Do pwm init code.
+    call RobotDoInit
+    
+    ; Set to zero for start
+    movlw 0x00
+    movwf PWMCONL
+    
+    movlw 0x00
+    movwf PWMCONR
+    
+    bcf TRISB,RB5
+    bsf PORTB,RB5
+    clrf RobotState
+    
+    clrf LastSensL
+MainL: movff SensLastL, CurSensL
+    movf CurSensL, 0
+    xorwf LastSensL, 0		
+    bz MainL
+    
+    btfsc RobotState,0 ; we are in forward state unless this is set
+    bra Backup
+    
+    bsf PORTB,RB5 ; turn on light since we are in forward state ;
+    movlw .2 
+    cpfslt CurSensL ; continue to go forward unless we should backup
+    bra MainL
+    
+    ; if we are here we should backup
+    movlw 0xA4
+    movwf PWMCONL
+    
+    movlw 0xA4
+    movwf PWMCONR
+    
+    bsf RobotState,0 ; okay backing up after this
+    bra MainL
+    
+Backup:
+    bcf PORTB,RB5 ; okay kill the light we are backing up
+    
+    movlw .2
+    cpfsgt CurSensL ; keep backing up untill greater than 6 away
+    bra MainL
+    
+    ; we are here so both sides say we are good so lets go forward
+    movlw 0xE4
+    movwf PWMCONL
+    
+    movlw 0xE4
+    movwf PWMCONR
+    
+    bcf RobotState,0
+    bra MainL
+    
+    end
+
+
+
+
